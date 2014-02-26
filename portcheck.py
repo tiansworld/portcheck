180e3db3dfe55185a548e3ced2c2750f98ef7040
Tiansworld <tiansworld@fedoraproject.org>
2014-02-26 17:21:22 +0800

    Initial commit

---
+++ portcheck.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+
+## 
+## Note: This script is used for checking opened ports for an address(domain name or ip address).
+## It's convenient for you to check whether a port is opened on your computer, server
+## or something else. It's a simple tool aims to improve security.
+## Don't use it to do evil things.
+
+## About: This is my second python script. You know I am just a python newbie. Any
+## suggestion, improvement on this script is welcome.
+##
+
+## 
+import socket
+import argparse
+import sys
+
+def check_open_port(address='localhost', *ports):
+    port_lst = []
+    for i in ports:
+        print('Attempting to connect to {0} on port {1}'.format(address, i))
+
+        try:
+            socket.create_connection((address,i))
+            print("Connected to {0} on port {1}".format(address,i))
+            port_lst.append(i)
+        except socket.error as e:
+            print("Connection to {0} on port {2} failed {1}".format(address,e,i))
+            print("Port {0} seems not open on {1}".format(i,address))
+    if len(port_lst) == 0:
+        print("The port(s) you check: {0} is/are not opened".format(str(ports)[1:-1]))
+    else:
+        print("Opened Ports on {0} are: {1}".format(address,port_lst))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check open ports for ADDRESS')
+    parser.add_argument('-a',default='localhost', dest='address',
+                        metavar='ADDRESS', type=str,
+                        help='Address to be checked, either domain name or ip address, default is "localhost".')
+    parser.add_argument('-p', '--port',nargs='*',type=int,
+                        dest='ports',default=(22,80),
+                        metavar='PORTS', help='PORTS  to be checked')
+    args = parser.parse_args()
+    check_open_port(args.address,*args.ports)
